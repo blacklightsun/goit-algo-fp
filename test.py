7df34bdc0005b8180630615cd22d780957a6eacf
blacklightsun <blacklightsun@gmail.com>
2024-06-06 10:37:56 +0300

    task3 is ready

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import networkx as nx
-import heapq
 
 # дані для побудови схеми (вихідний малюнок тут же у теці)
 crossing_list = [
@@ -25,6 +24,7 @@ crossing_list = [
     ("Горіхівська", "Цецино"),
     ("Цецино", "Університет"),
 ]
+
 edge_lenght_list = [2.5, 7.0, 3.0, 1.5, 4.0,
                     2.0, 3.0, 5.0, 1.5, 2.0,
                     6.0, 1.0, 5.5, 3.0, 1.5,
@@ -38,30 +38,29 @@ for i, edge in enumerate(crossing_list):
 
 def dijkstra(graph, start_node):
     # функція написана по опису алгоритму з конспекту, без підглядання в код з конспекту
-    # upd: вибір найближчої ноди реалізований за допомогою бінарної купи
-    dist_dict = {node: float('inf') for node in graph.nodes}  # початковий словник відстаней
+    dist_dict = {node: float('inf') for node in graph.nodes}
     dist_dict[start_node] = 0
+    unvisited_list = list(graph.nodes)
 
-    unvisited_heap = [(float('inf'), node) for node in graph.nodes]  # початкова купа невідвіданих нод
-    heapq.heapify(unvisited_heap)  # з даними про відстань до них
-
-    while unvisited_heap:
-        for node in graph[start_node]:  # оновлення відстаней від поточної ноди
+    while unvisited_list:
+        # print(start_node)
+        # print(unvisited_list)
+        for node in graph[start_node]:
             if dist_dict[node] > dist_dict[start_node] + graph[start_node][node]['lenght']:
                 dist_dict[node] = dist_dict[start_node] + graph[start_node][node]['lenght']
+        # print(dist_dict)
+        unvisited_list.pop(unvisited_list.index(start_node))
 
-        i_for_pop = None
-        for i in range(len(unvisited_heap)):
-            unvisited_heap[i] = (dist_dict[unvisited_heap[i][1]], unvisited_heap[i][1])  # сінхронизація купи та словника відстаней
-            if unvisited_heap[i][1] == start_node:  # пошук індексу відвіданої ноди для видалення її з купи
-                i_for_pop = i
-
-        if i_for_pop:
-            unvisited_heap.pop(i_for_pop)  # видалення відвіданої ноди
-        heapq.heapify(unvisited_heap)  # перебудова купи після видалення відвіданої ноди
-
-        # пошук найближчої ноди для переходу
-        start_node = heapq.heappop(unvisited_heap)[1]
+        # пошук найближчої ноди
+        near_node = None
+        near_node_distance = float('inf')
+        # for node in graph[start_node]:
+        for node in dist_dict:
+            if dist_dict[node] < near_node_distance and node in unvisited_list:
+                near_node = node
+                near_node_distance = dist_dict[node]
+        if near_node is not None:
+            start_node = near_node
 
     return dist_dict
 
